972a72f35a6ef891a63a150b29306b63876cba02
wang <wangb@shure.com>
2021-01-25 23:31:54 +0800

    update test_pointer
    Signed-off-by: wang <wangb@shure.com>

--- VS_Test/LiNing_Course/pointer/test_pointer.cpp
+++ VS_Test/LiNing_Course/pointer/test_pointer.cpp
@@ -8,59 +8,49 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
+  int n = 100;
+  float value = 1.2;
 
-  enum color
-  {
-    red,            //default is 0
-    yellow,         //default is 1
-    green,
-    blue
-  };
+  int* pn = &n;
+  float* pvalue = &value;
 
-  enum COLOR        // set user defined value
-  {
-    RED     = 2,           
-    YELLOW  = 4, 
-    GREEN   = 6, 
-    BLUE    = 8
-  };
+  cout << "pn = " << pn << endl;
+  cout << "pvalue = " << pvalue << endl;
+  cout << "*pn = " << *pn << endl;
+  cout << "*pvalue = " << *pvalue << endl;
+
+  cout << "sizeof of pn is " << sizeof(pn) << endl;    // sizeof of pn  is 8
+  cout << "sizeof of *pn is " << sizeof(*pn) << endl;    // sizeof of *pn  is 4
 
 
-  color flowercolor;
-  flowercolor = blue;
+//int numbers[2] = { 25, 26 };   //ok
+  int numbers[2] { 25, 26 };   //ok
+  cout << "numbers[0] is " << *numbers << endl;
+  cout << "numbers[1] is " << *(numbers + 1) << endl;
 
-  COLOR FLOWERCOLOR;
-  COLOR FLOWERCOLOR2;
-  FLOWERCOLOR = GREEN;
-  FLOWERCOLOR2 = BLUE;
+  int* p = numbers;
+  cout << "p[0] is " << *p << endl;
+  cout << "p[1] is " << *(p + 1) << endl;
 
-  cout << "flower color is " << flowercolor << endl;
-  cout << "FLOWER COLOR is " << FLOWERCOLOR << endl;
-  cout << "FLOWER2 COLOR is " << FLOWERCOLOR2 << endl;
+  int xyz = 30;
+  int*q = &xyz;
+  *q = 123;
+  cout << "*q is " << *q << endl;
+  cout << "xyz is " << xyz << endl;
 
-  //enum class  C++ 11 new features
-  enum class number3{ONE,TWO,THREE};
-  enum class number4{ONE,TWO,THREE};
-  enum class number5{ONE,TWO,THREE,FOUR,FIVE};
-  enum class number6:short{ONE,TWO,THREE,FOUR,FIVE};      // the default element size is changed from 'int' to 'short', which is 4 to 2
+  // new and delete 
+  // typename *pointer = new typename
+  int *pointer = new int;
+  *pointer = 123;
+  cout << "*pointer is " << *pointer << " before delete" << endl;
+  cout << "*pointer address is " << pointer << " before delete" << endl;
+  delete pointer;
+  cout << "*pointer is " << *pointer << " after delete" << endl;
+  cout << "*pointer address is " << pointer << " after delete" << endl;
 
-  number3 num3 = number3::TWO;
-  number4 num4 = number4::TWO;
-
-  cout << "num3 is " << (int)num3 << endl;
-
-  cout << "number3 size is " << sizeof(number3) << endl;   //default each element size is 4
-  cout << "number5 size is " << sizeof(number5) << endl;   //default each element size is 4 
-  cout << "number6 size is " << sizeof(number6) << endl;   //default each element size is 2 
-
-#if 0
-  if ( num3 == number3::ONE)
-    cout << "num3 is ONE" << endl;
-  else if ( num3 == number3::TWO)
-    cout << "num3 is TWO" << endl;
-  else
-    cout << "num3 is THREE" << endl;
-#endif
-
+  double *ptest = new double;
+  *ptest = 123.4444;
+  cout << "*ptest is " << *ptest << endl;
+  delete ptest;
 
 }
