e8f68625a0420ac60d984ae947f069ff035ee4ae
wangben85 <wangben85@sina.com>
2021-02-02 15:22:54 +0800

    add test_class
    Signed-off-by: wangben85 <wangben85@sina.com>

---
+++ VS_Test/LiNing_Course/class/test_class.cpp
@@ -0,0 +1,137 @@
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include "test_class.hpp"
+
+using namespace std;
+
+//constructor
+Person::Person()
+{
+}
+
+//deconstructor
+Person::~Person()
+{
+
+}
+
+string Person::getName() const
+{
+    return m_Name;     
+}
+int Person::getCode() const
+{
+    return m_Code;
+}
+void Person::setName(string name)
+{
+   m_Name = name;    
+}
+void Person::setCode(int code)
+{
+  m_Code = code;
+}
+
+//inline function of the class Person
+inline void Person::inlineTest()
+{
+  cout << " This is the test of inline function" << endl;
+}
+
+//default constructor
+MyClass::MyClass()
+{
+   m_pMan = new Person();
+   cout << "MyClass::MyClass()" << endl;
+}
+
+//user constructor
+MyClass::MyClass( int code , string name)
+{
+   m_pMan = new Person();
+   this->code = code;
+   this->name = name;
+   cout << "MyClass::MyClass(int code, string name)" << endl;
+   cout << "MyClass code is " << code << endl; 
+   cout << "MyClass name is " << name << endl; 
+}
+
+#if 1
+//user constructor
+MyClass::MyClass( int code)
+{
+  m_pMan = new Person();
+  this->code = code;
+  cout << "MyClass::MyClass(int code)" << endl;
+  cout << "Only MyClass code is " << code << endl; 
+  // m_pMan->setCode(666);
+  // cout << "m_pMan getCode is " << m_pMan->getCode() << endl;
+}
+#endif
+
+#if 0 // why not work ?
+MyClass::MyClass( string str)
+{
+   name = str;
+   cout << "MyClass::MyClass(string name)" << endl;
+   cout << "Only MyClass name is " << name << endl; 
+}
+#endif
+
+MyClass* MyClass::getObject()
+{
+   return this;
+}
+
+MyClass::~MyClass()
+{
+    delete m_pMan;
+    cout << "MyClass::~MyClass(int code) is called" << endl;
+}
+
+int main(int argc, const char *argv[])
+{
+  // p1
+   Person p1;
+   p1.setName("Wang Ben");
+   p1.setCode(100);
+   cout << "p1.name = " << p1.getName() << endl;
+   cout << "p1.code = " << p1.getCode() << endl;
+   p1.inlineTest();
+
+   // p2
+   Person* p2 = new Person();
+   p2->setName("Jay CHOU");
+   p2->setCode(101);
+   cout << "p2->name = " << p2->getName() << endl;
+   cout << "p2->code = " << p2->getCode() << endl;
+   p2->inlineTest();
+   delete p2;
+
+   // p3
+   MyStruct p3 = { 202, "test"};
+   cout << "p3->name = " << p3.name << endl;
+   cout << "p3->code = " << p3.code << endl;
+
+   // p4,p5
+   MyClass p4;
+   MyClass p5( 1000, "Jay");
+
+   // p6
+   MyClass p6( 2000 );
+
+   // p7
+   MyClass* p7 = new MyClass( 505, "505Test");
+   delete p7;
+
+   // p8
+   MyClass* p8 = new MyClass();
+   if (p8->getObject()  == p8 ) 
+   {
+        cout << "equal" << endl;
+   }
+
+   return 0;
+}
---
+++ VS_Test/LiNing_Course/class/test_class.hpp
@@ -0,0 +1,70 @@
+#ifndef TEST_CLASS_HPP
+#define TEST_CLASS_HPP 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+public:
+    Person();
+    virtual ~Person();
+    string getName() const;
+    int getCode() const;
+    void setName(string name);
+    void setCode(int code);
+    void inlineTest(); // it is the inline function implementation in cpp file
+
+private: 
+    int m_Code;
+    string m_Name;
+};
+
+typedef struct 
+{
+   int code; 
+   string name;
+}MyStruct;
+
+class MyClass
+{
+public:
+  // constructors
+  // 1. constructor name is the same as the class 
+  // 2. no return value
+  MyClass();  // default constructor
+  // user defined constructors
+  MyClass(int code , string name);
+  MyClass(int code);
+//   Myclass(const string str);
+  
+  //deconstructor
+  // 1. no return value
+  // 2. no parameter
+  ~MyClass();
+
+  //function memebers
+  MyClass *getObject();
+
+
+private:
+  const int months = 12;
+  enum 
+  {
+    months1 = 12
+  };
+  static const int months2 = 12;
+  
+  int code;
+  string name;
+  Person* m_pMan;
+
+  // float values[months]; // normal variable not work
+  float values1[months1]; // enum could work
+  float values2[months2]; // static variable could work
+};
+
+
+#endif
